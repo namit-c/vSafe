68839b6ed9cc30f7beafe01bb5f577dc0267b893
Andrew Balmakund <balmakua@mcmaster.ca>
2020-02-20 17:07:18 -0500

    added Binary Search for Strings

---
+++ src/BinarySearch.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+public class BinarySearch{
+    public static int binarySeach(String[] arr, String str){
+        int low = 0;
+        int hi = arr.length -1;
+        while (low <= hi){
+            int mid = low + (hi-low)/2;
+            if (str.compareTo(arr[mid]) < 0){
+                hi = mid - 1;
+            }
+            else if (str.compareTo(arr[mid]) > 0){
+                low = mid +1;
+            }
+            else{
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+       //System.out.println("HELLO WORLD");
+       
+       String[] arr = {"ABC","DEF","GEF","Z"};
+       System.out.println(binarySeach(arr,"GEF"));
+       System.out.println(binarySeach(arr,"ABCD"));
+    } 
+}
+
