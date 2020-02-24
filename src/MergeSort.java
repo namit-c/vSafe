f29db5de8c70c5180bd4ed6134cd2a5b5b69474e
Andrew Balmakund <balmakua@mcmaster.ca>
2020-02-24 16:59:14 -0500

    redesigned Merge Sort to sort on a 2D String array that sorts only by a specific column

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -6,47 +6,47 @@ public class MergeSort{
      * on [Pg 271 - 273]
     */
 
-    private static String[] aux;
-    public static void sort(String[] a){
-        aux = new String[a.length];
-        sort(a,0,a.length -1);
+    private static String[][] aux;
+    public static void sort(String[][] a,int index){
+        aux = new String[a.length][a[0].length];
+        sort(a,0,a.length -1,index);
     }
     
-    private static void merge(String[] a, int low, int mid, int hi){
+    private static void merge(String[][] a, int low, int mid, int hi,int index){
         int i = low;
         int j = mid + 1;
         for (int k = low; k <= hi; k++){
-            aux[k] = a[k];
+            aux[k][index] = a[k][index];
         }
         for (int k = low; k <= hi; k++){
             if (i > mid){
-                a[k] = aux[j++];
+                a[k][index] = aux[j++][index];
             }
             else if (j > hi){
-                a[k] = aux[i++];
+                a[k][index] = aux[i++][index];
             }
-            else if (aux[j].compareTo(aux[i]) < 0){
-                a[k] = aux[j++];
+            else if (aux[j][index].compareTo(aux[i][index]) < 0){
+                a[k][index] = aux[j++][index];
             }
             else{
-                a[k] = aux[i++];
+                a[k][index] = aux[i++][index];
             }
         }
     }
-    private static void sort(String[] a, int low, int hi){
+    private static void sort(String[][] a, int low, int hi,int index){
         if (hi <= low){
             return;
         }
         int mid = low + (hi - low)/2;
-        sort(a,low,mid);
-        sort(a,mid+1,hi);
-        merge(a,low,mid,hi);
+        sort(a,low,mid,index);
+        sort(a,mid+1,hi,index);
+        merge(a,low,mid,hi,index);
     }
     
     public static void main(String[] args){
        String[] unsortedArr = {"E","Z","F","A","B","D"};
-       sort(unsortedArr);
-       System.out.println(Arrays.toString(unsortedArr));
+       //sort(unsortedArr);
+       //System.out.println(Arrays.toString(unsortedArr));
 
     }
 
