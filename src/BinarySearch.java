50cc06606787f8b43a6842e889b04cd10b624983
Andrew Balmakund <balmakua@mcmaster.ca>
2020-03-08 21:25:40 -0400

    added another method to perform the same search but with arrayList to help with performing binary search to remove repeated values

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,19 +1,20 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 public class BinarySearch{
 	 /*
      * From Algorithms 4th Edition Textbook by Robert Sedgewick and Kevin Wayne 
      * on [Pg 9]
     */
 
-    public static int binarySeach(String[] arr, String str){
+    public static int binarySearch(String[][] arr, String str, int indexCol){
         int low = 0;
         int hi = arr.length -1;
         while (low <= hi){
             int mid = low + (hi-low)/2;
-            if (str.compareTo(arr[mid]) < 0){
+            if (str.compareTo(arr[mid][indexCol]) < 0){
                 hi = mid - 1;
             }
-            else if (str.compareTo(arr[mid]) > 0){
+            else if (str.compareTo(arr[mid][indexCol]) > 0){
                 low = mid +1;
             }
             else{
@@ -24,12 +25,31 @@ public class BinarySearch{
         return -1;
     }
 
+    public static int binarySearch(ArrayList<String[]> arr, String str, int indexCol){
+        int low = 0;
+        int hi = arr.size() -1;
+        while (low <= hi){
+            int mid = low + (hi-low)/2;
+            if (str.compareTo(arr.get(mid)[indexCol]) < 0){
+                hi = mid - 1;
+            }
+            else if (str.compareTo(arr.get(mid)[indexCol]) > 0){
+                low = mid +1;
+            }
+            else{
+                return mid;
+            }
+        }
+
+        return -1;
+    }
     public static void main(String[] args){
        //System.out.println("HELLO WORLD");
        
-       String[] arr = {"ABC","DEF","GEF","Z"};
-       System.out.println(binarySeach(arr,"GEF"));
-       System.out.println(binarySeach(arr,"ABCD"));
+       //String[] arr = {"ABC","DEF","GEF","Z"};
+       //System.out.println(binarySeach(arr,"GEF"));
+       //System.out.println(binarySeach(arr,"ABCD"));
+      
     } 
 }
 
