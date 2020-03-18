a6c2facf3766acd90cf56999634c996d36e20d2d
Andrew Balmakund <balmakua@mcmaster.ca>
2020-03-18 11:37:51 -0400

    made a temporary fix to swap the entire rows in mergesort instead of swapping a specific column.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -16,21 +16,45 @@ public class MergeSort{
         int i = low;
         int j = mid + 1;
         for (int k = low; k <= hi; k++){
-            aux[k][index] = a[k][index];
+            for (int col = 0; col < a[0].length; col++){
+
+                aux[k][col] = a[k][col];
+            }
         }
         for (int k = low; k <= hi; k++){
             if (i > mid){
-                a[k][index] = aux[j++][index];
+                //a[k][index] = aux[j++][index];
+                insertRow(a,aux,k,j);
+                j++;
             }
             else if (j > hi){
-                a[k][index] = aux[i++][index];
+                //a[k][index] = aux[i++][index];
+                insertRow(a,aux,k,i);
+                i++;
             }
             else if (aux[j][index].compareTo(aux[i][index]) < 0){
-                a[k][index] = aux[j++][index];
+                //a[k][index] = aux[j++][index];
+                insertRow(a,aux,k,j);
+                j++;
             }
             else{
-                a[k][index] = aux[i++][index];
+                //a[k][index] = aux[i++][index];
+                insertRow(a,aux,k,i);
+                i+=1;
             }
+        }
+
+    }
+
+    //The following does a shallow copy of the contents of row in the second matrix and stores it in the firstMatrix
+    //Previously in the merge function, if a[k] = aux[j++] for example, this would lose pointers to the previous rows and be lossed
+    //lossed in memory. This solution prevents it from happening, however this heavily impacts performance.
+    //
+    //Need to find an efficient way to clone rows.
+    private static void insertRow(String[][] firstMatrix, String[][] secondMatrix,int firstRow, int secondRow){
+        for (int i = 0; i < secondMatrix[secondRow].length; i++){
+            String temp = new String(secondMatrix[secondRow][i]);
+            firstMatrix[firstRow][i] = temp;
         }
     }
     private static void sort(String[][] a, int low, int hi,int index){
